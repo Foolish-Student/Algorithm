1867b1b056edd97fa6c90968a11e0edbf9405103
hello-liwan <58982538+hello-liwan@users.noreply.github.com>
2020-03-01 15:18:01 +0800

    Add files via upload

---
+++ 筛选法求素数(埃拉托斯特尼筛法)模板.cpp
@@ -0,0 +1,28 @@
+﻿//10000以内的素数
+#include<iostream>
+using namespace std;
+void prime();
+bool number[10000];
+int main()
+{
+	prime();
+	for (int i = 2; i <= 10000; i++)
+	{
+		if (!number[i])
+			cout << i << " ";
+	}
+	return 0;
+}
+void prime()
+{
+	for (int i = 2; i <= 10000; i++)
+	{
+		if (!number[i])
+		{
+			for (int j = i * i; j <= 10000; j += i)
+			{
+				number[j] = true;
+			}
+		}	
+	}
+}
